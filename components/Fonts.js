e6810a1af574df7dc8057fda23f4bfe45d29b6dc
Shernan Javier <shernanjavier@gmail.com>
2021-12-29 15:37:40 -0500

    GAOSDJ

--- components/Fonts.js
+++ components/Fonts.js
@@ -9,7 +9,7 @@ export default createGlobalStyle`
     padding: 0;
     height: 100%;
     width: 100%;
-  }
+  };
 
   body{
     padding: 0;
@@ -19,9 +19,20 @@ export default createGlobalStyle`
     font-style: normal;
     height: 100vh;
     width: 100%;
-  }
+  };
   @font-face {
     font-family: "Filson Pro";
     src: url("/fonts/FilsonProMedium.otf") format("opentype");
-  }
+    font-weight: normal; 
+  };
+  @font-face {
+    font-family: "Proxima Nova";
+    src: url("/fonts/ProximaNova-Regular.otf") format("opentype");
+    font-weight: normal; 
+  };
+  @font-face {
+    font-family: "Proxima Nova";
+    src: url("/fonts/ProximaNovaAlt-Bold.otf") format("opentype");
+    font-weight: bold; 
+  };
 `;
