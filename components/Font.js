e2d4f1d43e4f92f4793e41c987e925c55a491d4e
Shernan Javier <shernanjavier@gmail.com>
2021-12-29 13:39:19 -0500

    Plz work aaaa

--- components/Font.js
+++ components/Font.js
@@ -2,24 +2,24 @@ import { createGlobalStyle } from "styled-components";
 
 export default createGlobalStyle`
   @font-face {
-  font-family: "Filson Pro";
-  src: url("/fonts/Filson Pro/FilsonProMedium.otf");
+  font-family: "FilsonPro";
+  src: url("/fonts/FilsonPro/FilsonProMedium.otf") format("opentype");
   font-style: normal;
   font-weight: 500;
   font-display: swap;
 }
 
 @font-face {
-  font-family: "Proxima Nova";
-  src: url("/fonts/Proxima Nova/ProximaNova-Regular.otf");
+  font-family: "ProximaNova";
+  src: url("/fonts/ProximaNova/ProximaNova-Regular.otf") format("opentype");
   font-style: normal;
   font-weight: normal;
   font-display: swap;
 }
 
 @font-face {
-  font-family: "Proxima Nova";
-  src: url("/fonts/Proxima Nova/ProximaNovaAlt-Bold.otf");
+  font-family: "ProximaNova";
+  src: url("/fonts/ProximaNova/ProximaNovaAlt-Bold.otf") format("opentype");
   font-style: normal;
   font-weight: bold;
   font-display: swap;
