06b379eb0d2c64297e8fbfc438a1f41362868733
Shernan Javier <shernanjavier@gmail.com>
2021-12-29 14:13:07 -0500

    font style jsx global

--- pages/_app.js
+++ pages/_app.js
@@ -68,6 +68,33 @@ const theme = {
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <style jsx global>{`
+        @font-face {
+          font-family: "FilsonPro";
+          src: url("/fonts/FilsonPro/FilsonProMedium.otf") format("opentype");
+          font-style: normal;
+          font-weight: 500;
+          font-display: swap;
+        }
+
+        @font-face {
+          font-family: "ProximaNova";
+          src: url("/fonts/ProximaNova/ProximaNova-Regular.otf")
+            format("opentype");
+          font-style: normal;
+          font-weight: normal;
+          font-display: swap;
+        }
+
+        @font-face {
+          font-family: "ProximaNova";
+          src: url("/fonts/ProximaNova/ProximaNovaAlt-Bold.otf")
+            format("opentype");
+          font-style: normal;
+          font-weight: bold;
+          font-display: swap;
+        }
+      `}</style>
       <GlobalStyle />
       <Font />
       <ThemeProvider theme={theme}>
--- pages/index.js
+++ pages/index.js
@@ -32,6 +32,33 @@ const Home = () => {
           crossOrigin=""
         />
       </Head>
+      <style jsx global>{`
+        @font-face {
+          font-family: "FilsonPro";
+          src: url("/fonts/FilsonPro/FilsonProMedium.otf") format("opentype");
+          font-style: normal;
+          font-weight: 500;
+          font-display: swap;
+        }
+
+        @font-face {
+          font-family: "ProximaNova";
+          src: url("/fonts/ProximaNova/ProximaNova-Regular.otf")
+            format("opentype");
+          font-style: normal;
+          font-weight: normal;
+          font-display: swap;
+        }
+
+        @font-face {
+          font-family: "ProximaNova";
+          src: url("/fonts/ProximaNova/ProximaNovaAlt-Bold.otf")
+            format("opentype");
+          font-style: normal;
+          font-weight: bold;
+          font-display: swap;
+        }
+      `}</style>
       <Hero />
       {/** About Section Component */}
       {/* <About /> */}
