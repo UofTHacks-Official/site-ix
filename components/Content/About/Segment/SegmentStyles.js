cc6c65dfd3c2a5b2a4f74835952c728710aed1eb
Shernan Javier <shernanjavier@gmail.com>
2022-01-02 01:45:29 -0500

    Made text responsive

--- components/Content/About/Segment/SegmentStyles.js
+++ components/Content/About/Segment/SegmentStyles.js
@@ -14,7 +14,6 @@ export const Title = styled.h3`
   line-height: 30px;
   align-items: center;
   margin-bottom: 0.53vh;
-  /* color: #242424; */
   color: white;
 `;
 
@@ -25,5 +24,4 @@ export const Paragraph = styled.p`
   font-size: 1.02rem;
   line-height: 22px;
   color: #f1f1f1;
-  /* color: #35363a; */
 `;
