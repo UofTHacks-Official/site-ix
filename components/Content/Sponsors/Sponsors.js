58b634a35d1d56bb8cc86cfa58fa5b46dc8cf7a1
Shernan Javier <shernanjavier@gmail.com>
2022-01-03 14:35:54 -0500

    Implemented sponsors section

--- components/Content/Sponsors/Sponsors.js
+++ components/Content/Sponsors/Sponsors.js
@@ -1,17 +1,105 @@
 import React from "react";
 import Divider from "../../Shared/Divider/Divider";
-import { Wrapper, Tier, SponsorContainer } from "./SponsorsStyles";
+import {
+  Wrapper,
+  Header,
+  Tier,
+  GoldSponsor,
+  SilverSponsor,
+  BronzeSponsor,
+  SponsorGrid,
+} from "./SponsorsStyles";
 import Image from "next/image";
+import Amazon from "/public/static/Sponsors/Amazon.svg";
+import Ubisoft from "/public/static/Sponsors/Ubisoft.svg";
+import FDM from "/public/static/Sponsors/FDM.svg";
+import Echo3D from "/public/static/Sponsors/echo3d.png";
+import Maplesoft from "/public/static/Sponsors/Maplesoft.png";
+import PnG from "/public/static/Sponsors/ProcterAndGamble.svg";
+import QuestTrade from "/public/static/Sponsors/QuestTrade.svg";
+import WolframLanguage from "/public/static/Sponsors/WolframLanguage.svg";
+import Clerky from "/public/static/Sponsors/Clerky.svg";
 
 const Sponsors = () => {
   return (
     <Wrapper>
       <Divider />
-      <h2>Our sponsors</h2>
-      <Tier>
-        <SponsorContainer></SponsorContainer>
-        <SponsorContainer></SponsorContainer>
-      </Tier>
+      <Header>Our sponsors</Header>
+      <SponsorGrid>
+        <Tier>
+          <GoldSponsor href="https://amazon.com/" target="_blank">
+            <Image
+              src={Amazon}
+              layout="fill"
+              objectFit="contain"
+              alt="Amazon"
+            />
+          </GoldSponsor>
+          <GoldSponsor href="https://ubisoft.com" target="_blank">
+            <Image
+              src={Ubisoft}
+              layout="fill"
+              objectFit="contain"
+              alt="Ubisoft"
+            />
+          </GoldSponsor>
+        </Tier>
+        <Tier>
+          <SilverSponsor href="https://www.fdmgroup.com/" target="_blank">
+            <Image src={FDM} layout="fill" objectFit="contain" alt="FDM" />
+          </SilverSponsor>
+          <SilverSponsor>
+            <Image
+              src={PnG}
+              layout="fill"
+              objectFit="contain"
+              alt="Procter and Gamble"
+            />
+          </SilverSponsor>
+          <SilverSponsor href="https://www.questrade.com/" target="_blank">
+            <Image
+              src={QuestTrade}
+              layout="fill"
+              objectFit="contain"
+              alt="QuestTrade"
+            />
+          </SilverSponsor>
+        </Tier>
+        <Tier>
+          <BronzeSponsor href="https://www.clerky.com/" target="_blank">
+            <Image
+              src={Clerky}
+              layout="fill"
+              objectFit="contain"
+              alt="Clerky"
+            />
+          </BronzeSponsor>
+          <BronzeSponsor href="https://www.echo3d.co/" target="_blank">
+            <Image
+              src={Echo3D}
+              layout="fill"
+              objectFit="contain"
+              alt="Echo3D"
+            />
+          </BronzeSponsor>
+          <BronzeSponsor href="https://www.maplesoft.com/" target="_blank">
+            <Image
+              src={Maplesoft}
+              layout="fill"
+              objectFit="contain"
+              alt="Maplesoft"
+            />
+          </BronzeSponsor>
+          <BronzeSponsor href="https://www.wolfram.com/" target="_blank">
+            <Image
+              src={WolframLanguage}
+              layout="fill"
+              objectFit="contain"
+              alt="Wolfram Language"
+            />
+          </BronzeSponsor>
+        </Tier>
+      </SponsorGrid>
     </Wrapper>
   );
 };
--- components/Content/Sponsors/SponsorsStyles.js
+++ components/Content/Sponsors/SponsorsStyles.js
@@ -8,15 +8,51 @@ export const Wrapper = styled.article`
   background-color: #f9f9f9;
 `;
 
-export const Tier = styled.div`
-  display: flex;
-  background-color: pink;
-  width: 71.375rem;
+export const Header = styled.h2`
+  font-family: "Filson Pro";
+  font-weight: normal;
+  color: #242424;
+  text-align: center;
+  font-size: 3rem;
+  margin-top: 2;
 `;
 
-export const SponsorContainer = styled.div`
+export const SponsorGrid = styled.div`
+  padding: 2rem;
+`;
+
+export const Tier = styled.div`
   display: flex;
-  flex: 1;
-  align-items: center;
-  -webkit-box-align: center;
+  flex-wrap: wrap;
+  -webkit-box-pack: center;
+  place-content: center;
+  box-sizing: border-box;
+  margin: 0 auto;
+  max-width: 71.135rem;
+  gap: 5rem;
+  padding-bottom: 5rem;
+`;
+
+export const GoldSponsor = styled.a`
+  position: relative;
+  height: 200px;
+  cursor: pointer;
+  max-width: 30em;
+  width: 100%;
+  border-radius: 15px;
+  transition: all 0.3s ease 0s;
+  &:hover {
+    transform: scale(1.05);
+    filter: drop-shadow(0 0 80px rgba(0, 0, 0, 0.25));
+  }
+`;
+
+export const SilverSponsor = styled(GoldSponsor)`
+  height: 125px;
+  max-width: 15rem;
+`;
+
+export const BronzeSponsor = styled(GoldSponsor)`
+  height: 80px;
+  max-width: 10rem;
 `;
