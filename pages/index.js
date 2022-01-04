18116d8c1a233656b840a4eb248a9c6468720680
Shernan Javier <shernanjavier@gmail.com>
2022-01-04 13:20:50 -0500

    Updated favicons

--- pages/index.js
+++ pages/index.js
@@ -16,13 +16,31 @@ const Home = () => {
   return (
     <Wrapper>
       <Head>
-        <title>UofTHacks IX Dev</title>
+        <title>UofTHacks IX</title>
         <meta charSet="UTF-8"></meta>
         <meta httpEquiv="X-UA-Compatible" content="IE=edge"></meta>
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1.0"
         ></meta>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest"></link>
       </Head>
       {/* Navigation Component Here */}
       <Main />
